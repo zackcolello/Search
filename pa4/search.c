0637a25f7cf13565b38ef3130d519145fa2fc9f2
Anna Genke <avg42@vi.cs.rutgers.edu>
2014-11-09 17:36:15 -0500

    jon snoo

--- pa4/search.c
+++ pa4/search.c
@@ -6,7 +6,34 @@
 #include "parsefile.h"
 #include "bst.h"
 
-void so(struct tokenNode* query, struct Tree* tree){
+
+void printList(struct List *list){
+
+	struct tokenNode *tempT;
+	struct fileNode *tempF;
+
+	tempT = list->head;
+	tempF = tempT->child;
+
+	while(tempT){
+		printf("%s\n", tempT->token);
+	
+		while(tempF){
+
+			printf("\t%s\n", tempF->path);
+			tempF = tempF->child;
+		}
+
+		tempT = tempT->sibling;
+
+		if(tempT!= NULL){
+		tempF = tempT->child;
+		}
+	}
+
+}
+
+struct fileNode* so(struct tokenNode* query, struct Tree* tree){
 
 	struct fileNode *resulthead = (struct fileNode*)malloc(sizeof(struct fileNode));
 	resulthead->child = NULL;
@@ -79,25 +106,11 @@ void so(struct tokenNode* query, struct Tree* tree){
 		ptr = ptr->sibling;
 	}
 	//print
-	printPtr=resulthead;
-	if (printPtr->path ==NULL){
-		printf("No results found.\n");
-		return;
-	}
-	while(printPtr){
-		if(printPtr->child == NULL){
-			printf("%s\n", printPtr->path);
-			break;
-		}
-
-		printf("%s, ",printPtr->path);
-		printPtr= printPtr->child;
-
-	}
-
+	
+	return resulthead;
 }
 
-void sa(struct tokenNode* query, struct Tree* tree){
+struct fileNode* sa(struct tokenNode* query, struct Tree* tree){
 	 
 	 struct List *ls= (struct List*)malloc(sizeof(struct List));
 	 ls->head=query;
@@ -178,31 +191,105 @@ void sa(struct tokenNode* query, struct Tree* tree){
 		ptr = ptr->sibling;
 	}
 	//print
-	printPtr=resulthead;
-	if (printPtr->path ==NULL){
-		printf("No results found.\n");
-		return;
-	}
-	while(printPtr){
-		if(printPtr->child == NULL){
-			if(printPtr->count==count){
-				printf("%s\n", printPtr->path);
-				printFlag=1;
-			}
-			break;
-		}
-		if(printPtr->count==count){
-			printf("%s, ",printPtr->path);
-			printFlag=1;
-		}
-		printPtr= printPtr->child;
-	}
-	if (!printFlag){
-		printf("No results found.\n");
-	}
+	return resulthead;
 
 
 }
+
+//Search NOR
+struct fileNode* sno(struct tokenNode* query, struct Tree* tree, struct fileNode* head){
+
+	printf("head is %s\n", head->path);
+
+	struct fileNode *temp;
+
+	temp = head;
+
+	while(temp){
+		printf("temp is: %s\n", temp->path);
+
+		temp = temp->child;
+
+	}
+
+
+	struct fileNode* soNode= so(query, tree);
+
+	if(soNode == NULL){
+		return NULL;
+	}
+
+}
+
+
+void printResult(struct fileNode* head, int flag, int count){
+
+	struct fileNode* printPtr;
+	printPtr=head;
+	int printFlag = 0;
+
+	switch(flag){
+	case 0:
+		printPtr=head;
+		if (printPtr->path ==NULL){
+			printf("No results found.\n");
+			return;
+		}
+		while(printPtr){
+			if(printPtr->child == NULL){
+				printf("%s\n", printPtr->path);
+				break;
+			}
+	
+			printf("%s, ",printPtr->path);
+			printPtr= printPtr->child;
+	
+		}
+		break;
+	
+	case 1:
+	
+		if (printPtr->path ==NULL){
+			printf("No results found.\n");
+			return;
+		}
+		while(printPtr){
+			if(printPtr->child == NULL){
+				if(printPtr->count==count){
+					if (!printFlag){
+						printf("%s", printPtr->path);
+						printFlag=1;
+					}else{
+						printf(", %s",printPtr->path);
+					}
+
+
+				}
+				break;
+			}
+			if(printPtr->count==count){
+			
+				if (!printFlag){
+						printf("%s", printPtr->path);
+						printFlag=1;
+					}else{
+						printf(", %s",printPtr->path);
+					}
+
+			}
+			printPtr= printPtr->child;
+		}
+		if (!printFlag){
+			printf("No results found.");
+		}
+		printf("\n");
+			break;
+	}
+
+
+
+}
+
 
 int main(int argc, char **argv){
 
@@ -227,10 +314,39 @@ int main(int argc, char **argv){
 
 	struct List* ls = buildLL(fp);	
 
-	
+	//make list of File nodes to be used later
+	struct tokenNode* Tptr = ls->head;
+	struct fileNode* Fptr = Tptr->child;
+
+	struct fileNode* temp = NULL;
+	struct fileNode* allfilehead = NULL;
+
+
+	while(Tptr){
+		while(Fptr){
+
+			temp = (struct fileNode*)malloc(sizeof(struct fileNode));
+			temp->path = (char*)malloc(strlen(Fptr->path)+1);
+			strcpy(temp->path, Fptr->path);
+
+			temp->child = allfilehead;
+			allfilehead = temp;
+
+			Fptr = Fptr->child;
+		}
+
+		Tptr = Tptr->sibling;
+
+		if(Tptr){
+			Fptr = Tptr->child;
+		}
+
+	}
+
+
 	tree->root = LLtoBST(ls);	
 //	printBST(tree->root);
-	
+
 	char* input= malloc(1000); //need to make sure input does not go out of bounds
 
 	char* token;
@@ -253,11 +369,14 @@ int main(int argc, char **argv){
 			continue;
 		}
 		
-		if(strcmp(token, "sa") == 0){
+		if(strcmp(token, "so") == 0){
 			flag = 0;		
-
-		}else if(strcmp(token, "so") == 0){
+		}else if(strcmp(token, "sa") == 0){
 			flag = 1;
+		}else if(strcmp(token, "sxo") == 0){
+			flag = 2;
+		}else if(strcmp(token, "sno") == 0){
+			flag = 3;
 		}else{
 			fprintf(stderr, "incorrect input.\n");
 			continue;
@@ -293,15 +412,29 @@ int main(int argc, char **argv){
 			token = strtok(NULL, " ");
 		}
 
+		//Make new list to find count
+		struct List *ls2 = (struct List*)malloc(sizeof(struct List));
+		ls2->head = head;
+		struct fileNode* result;
+		int count = 0;
+
 		switch(flag){
-		case 0:
-			sa(head, tree);	
+		case 0: //SO
+			result = so(head, tree);
+			printResult(result, 0, 0);
 			break;
 
-		case 1:
-			so(head, tree);
+		case 1: //SA
+			count = countNodes(ls2);
+			result = sa(head, tree);
+			printResult(result, 1, count);
 			break;
-
+		case 2: //SXOR
+			result = sa(head, tree);
+			printResult(result, 1, 1);
+			break;
+		case 3: //SNO
+			result = sno(head, tree, allfilehead);
 		}
 
 /* build list
